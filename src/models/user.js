03a2bb626d2dc50cb09b9ff38f6183e22878cea3
Roberto Bocio Melo <elpicapica01@gmail.com>
2022-01-10 20:58:05 -0500

    refactor(Root): cambio en los datos del usuario

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,10 @@
 import { model, Schema } from 'mongoose'
 
 const userSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true }
+  name: { type: String, required: true },
+  // password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  perfilURL: { type: String, required: false }
 }, {
   timestamps: true,
   versionKey: false
