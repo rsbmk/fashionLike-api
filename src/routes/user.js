846f071d5efe657fcb6c8efb014cd7b736f0c946
Markos <markosmk@me.com>
2022-02-24 13:03:02 -0300

    add: refactorizacion de rutas para add middlew

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,55 +1,20 @@
 import { Router } from 'express'
-import { User } from '../models/user'
-import validEmail from '../utils'
 const router = Router()
 
+// controllers
+import { register } from '../controllers/auth'
+import { getAllUsers, getUserByEmail } from '../controllers/user'
+
+// middlewares
+// import { validID } from '../middleware/validID'
+
 // create a user
-router.post('/users/register', async (req, res, next) => {
-  const { name, email, perfilURL } = req.body
-
-  const isValidEmail = validEmail(email)
-  if (!name || !email || !perfilURL || !isValidEmail) {
-    return res.status(400).json({ error: 'Missing fields' })
-  }
-
-  try {
-    const userExist = await User.findOne({ email })
-    if (userExist) return res.status(401).json({ error: 'User already exists', userExist })
-
-    const user = await new User({ name, email, perfilURL })
-    await user.save() // save user in DB
-    res.status(201).json(user)
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/users/register', register)
 
 // get all user
-router.get('/users', async (req, res) => {
-  const allUsers = await User.find({})
-  if (!allUsers.length) return res.status(404).json({ message: 'User not found' })
+router.get('/users', getAllUsers)
 
-  res.status(200).json(allUsers)
-})
-
-// get a user by id
-router.get('/users/:email', async (req, res, next) => {
-  const { email } = req.params
-  const isValidEmail = validEmail(email)
-
-  if (!email || !isValidEmail) {
-    return res.status(400).json({ error: 'Missing fields' })
-  }
-
-  try {
-    const user = await User.findOne({ email })
-
-    if (!user) res.status(404).json({ message: 'User not found' })
-
-    res.status(200).json(user)
-  } catch (error) {
-    next(error)
-  }
-})
+// get a user by email
+router.get('/users/:email', getUserByEmail)
 
 export default router
